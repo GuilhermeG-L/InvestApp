c2e03dddeaaaf68140464c83d1d1a92431fd7cbf
Guilherme Lima <93679190+GuilhermeG-L@users.noreply.github.com>
2022-06-10 18:25:45 -0300

    Registrar Conta + Ajustes

--- regConta.js
+++ regConta.js
@@ -6,10 +6,158 @@ const teste = urlParams.get('teste'); // TIRAR TESTE DEPOIS
 const user = urlParams.get('user');
 const conta = urlParams.get('conta');
 
-function converterData(data){
-  var arrData = data.split('-')
-  var [ano, mes, dia] = arrData
-  return `${ano}${mes}${dia}`
+console.log(conta);
+
+if (conta != null) {
+
+  var btnRegistrar = document.querySelector('.btn-registrar');
+  btnRegistrar.addEventListener('click', ()=>{
+    let nomeconta = document.querySelector('#input1').value;
+    let agencia = document.querySelector('#input3').value;
+    let numero = document.querySelector('#input4').value;
+    let banco = document.querySelector('#input5').value;
+    let pix = document.querySelector('#input6').value;
+    let rendimento = parseFloat(document.querySelector('#input7').value);
+    
+
+    // Configuração de conexão DB.
+    const config = {
+      authentication: {
+        options: {
+          userName: "sqlserver",
+          password: "IES301%Inve$tApp"
+        },
+        type: "default"
+      },
+      server: "35.199.92.55",
+      options: {
+        database: "InvestApp",
+        encrypt: true
+      }
+    };
+
+    // Query Azure SQL: Busca Produto.
+    const connection = new Connection(config);
+
+    // Tentativa de conexão.
+    connection.on("connect", err => {
+      if (err) {
+        ipc.send('erroconexao');
+      } else {queryDatabase();}
+    });
+
+    // Conexão do DB.
+    connection.connect();
+
+    // Função de criação de Query.
+    function queryDatabase() {
+      console.log("Lendo dados da tabela...");
+
+      const request = new Request(
+        `UPDATE dbo.Conta
+        SET NomeConta = \'${nomeconta}\', NomeBanco = \'${banco}\', Agencia = \'${agencia}\', Numero = \'${numero}\', PIX = \'${pix}\', Rendimento = \'${rendimento}\'
+        Where CodUsuario = \'${user}\' AND NomeConta = \'${conta}\'`,
+        (err, rowCount) => {
+          if (err) {
+            ipc.send('erroupdate');
+            console.error(err.message);
+          }
+          else {
+            console.log(`${rowCount} linha(s) retornadas`);
+            if (rowCount != 1) {ipc.send('erroupdate');}
+            else {window.location = "../Conta/Conta.html?user="+user;}
+          }
+        }
+      );
+    
+
+      request.on("row", columns => {
+        columns.forEach(column => {
+          valInput = ("%s\t%s", /*column.metadata.colName,*/ column.value);
+          console.log(valInput);
+        });
+      });
+
+      connection.execSql(request);
+    }})
+
+}
+
+else {
+
+  var btnRegistrar = document.querySelector('.btn-registrar');
+  btnRegistrar.addEventListener('click', ()=>{
+    let nomeconta = document.querySelector('#input1').value;
+    let saldo = parseFloat(document.querySelector('#input2').value);
+    let agencia = document.querySelector('#input3').value;
+    let numero = document.querySelector('#input4').value;
+    let banco = document.querySelector('#input5').value;
+    let pix = document.querySelector('#input6').value;
+    let rendimento = parseFloat(document.querySelector('#input7').value);
+    
+
+    // Configuração de conexão DB.
+    const config = {
+      authentication: {
+        options: {
+          userName: "sqlserver",
+          password: "IES301%Inve$tApp"
+        },
+        type: "default"
+      },
+      server: "35.199.92.55",
+      options: {
+        database: "InvestApp",
+        encrypt: true
+      }
+    };
+
+    // Query Azure SQL: Busca Produto.
+    const connection = new Connection(config);
+
+    // Tentativa de conexão.
+    connection.on("connect", err => {
+      if (err) {
+        ipc.send('erroconexao');
+      } else {queryDatabase();}
+    });
+
+    // Conexão do DB.
+    connection.connect();
+
+    // Função de criação de Query.
+    function queryDatabase() {
+      console.log("Lendo dados da tabela...");
+
+      const request = new Request(
+        `Insert Into dbo.Conta values (\'${nomeconta}\', \'${user}\', \'${banco}\', \'${saldo}\', \'${agencia}\', \'${numero}\', \'${pix}\', \'${rendimento}\');`,
+        (err, rowCount) => {
+          if (err) {
+            ipc.send('erroupdate');
+            console.error(err.message);
+          }
+          else {
+            console.log(`${rowCount} linha(s) retornadas`);
+            if (rowCount != 1) {ipc.send('erroinsert');}
+            else {window.location = "../Conta/Conta.html?user="+user;}
+          }
+        }
+      );
+    
+
+      request.on("row", columns => {
+        columns.forEach(column => {
+          valInput = ("%s\t%s", /*column.metadata.colName,*/ column.value);
+          console.log(valInput);
+        });
+      });
+
+      connection.execSql(request);
+    }})
+
+
+
+  
 }
 
 function listagem() {
@@ -67,8 +215,13 @@ function listagem() {
     request.on("row", columns => {
       columns.forEach(column => {
         dado = ("%s\t%s", /*column.metadata.colName,*/ column.value);
-        console.log(dado);
-        document.getElementById(`input${i}`).value = dado;
+        document.getElementById('input2').readOnly = true;
+        document.getElementById('input2').style.color = '#C4C4C4';
+        if (column.metadata.colName == 'Saldo' || column.metadata.colName == 'Rendimento') {
+          dado = parseFloat(dado).toFixed(2);
+          document.getElementById(`input${i}`).value = dado; }
+        else {
+          document.getElementById(`input${i}`).value = dado; }
         i++;
       });
     });
