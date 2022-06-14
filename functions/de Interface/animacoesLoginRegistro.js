746294711c22a9d056db5a03a5e403a6753bd21c
ishida3103 <lucasishida31@gmail.com>
2022-06-14 10:12:39 -0300

    Manutenção da animação Login e Registro

--- functions/de Interface/animacoesLoginRegistro.js
+++ functions/de Interface/animacoesLoginRegistro.js
@@ -6,29 +6,66 @@ var linkLogin = document.querySelector('.link-login')
 
 
 linkRegistro.addEventListener('click', ()=>{
+    cd.style.right = '0'
+
+    cd.style.animationPlayState = 'paused'
+    cd.style.animationName = 'movimentaContainer'
+    
     primeiroConteudo.style.animationName = 'someConteudo'
     segundoConteudo.style.animationName = 'apareceConteudo'
-    cd.style.animationName = 'movimentaContainer'
 
-    cd.style.animationPlayState = 'running'
-    primeiroConteudo.style.animationPlayState = 'running'
-    segundoConteudo.style.animationPlayState = 'running'
-    setTimeout(()=>{console.log(segundoConteudo)}, 3000)
-
-})
-
-linkLogin.addEventListener('click', ()=>{
-    cd.style.animationPlayState = 'paused'
-    cd.style.animationName = 'retornaContainer'
-    cd.style.animationPlayState = 'running'
+    primeiroConteudo.style.opacity = '100'
+    primeiroConteudo.style.animatinDuration = '1s'
+    primeiroConteudo.style.animationDelay = '1s'
     
-    // primeiroConteudo.style.animationName = 'apareceConteudo'
-    // segundoConteudo.style.animationName = 'someConteudo'
-    // cd.style.animationName = 'retornaContainer'
+    segundoConteudo.style.opacity = '0'
+    segundoConteudo.style.animationDuration = '2s'
+    segundoConteudo.style.animationDelay = '4s'
+
+
+    primeiroConteudo.style.animationPlayState = 'running'
+    cd.style.animationPlayState = 'running'
+    segundoConteudo.style.animationPlayState = 'running'
+    // console.log(`Left: ${cd.style.left}`)
+    // console.log(`Right: ${cd.style.right}`)
+    // setTimeout(()=>{console.log(cd.style.animationName  )}, 3000)
+    console.log('Voltou para o login')
+
+
+    // primeiroConteudo.style.animationName = 'someConteudo'
+    // segundoConteudo.style.animationName = 'apareceConteudo'
+    // cd.style.animationName = 'movimentaContainer'
 
     // cd.style.animationPlayState = 'running'
     // primeiroConteudo.style.animationPlayState = 'running'
     // segundoConteudo.style.animationPlayState = 'running'
-    setTimeout(()=>{console.log(cd.style.animationName  )}, 3000)
+    // setTimeout(()=>{console.log(segundoConteudo)}, 3000)
 
+})
+
+linkLogin.addEventListener('click', ()=>{
+    cd.style.right = '50%'
+
+    cd.style.animationPlayState = 'paused'
+    cd.style.animationName = 'retornaContainer'
+    
+    primeiroConteudo.style.animationName = 'apareceConteudo'
+    segundoConteudo.style.animationName = 'someConteudo'
+
+    primeiroConteudo.style.opacity = '0'
+    primeiroConteudo.style.animatinDuration = '2s'
+    primeiroConteudo.style.animationDelay = '4s'
+    
+    segundoConteudo.style.opacity = '100'
+    segundoConteudo.style.animationDuration = '1s'
+    segundoConteudo.style.animationDelay = '1s'
+
+
+    segundoConteudo.style.animationPlayState = 'running'
+    cd.style.animationPlayState = 'running'
+    primeiroConteudo.style.animationPlayState = 'running'
+    // console.log(`Left: ${cd.style.left}`)
+    // console.log(`Right: ${cd.style.right}`)
+    // setTimeout(()=>{console.log(cd.style.animationName  )}, 3000)
+    console.log('Voltou para o login')
 })
